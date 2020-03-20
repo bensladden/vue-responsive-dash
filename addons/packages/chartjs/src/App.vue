e225aa3bbc2cfbc9f86cba8fdcd58a97eebaf35a
bensladden <ben.sladden@dematec.com.au>
2020-03-20 21:30:08 +1030

    tested build:lib on addons

--- addons/packages/chartjs/src/App.vue
+++ addons/packages/chartjs/src/App.vue
@@ -25,6 +25,7 @@
 <script>
 import { Dashboard, DashLayout, DashItem } from "vue-responsive-dash";
 import LineChart from "./components/LineChart.vue";
+import { layouts } from "../../common/dataItems";
 
 export default {
   name: "App",
@@ -38,91 +39,7 @@ export default {
     return {
       datacollection: null,
       options: null,
-      layouts: [
-        {
-          breakpoint: "xl",
-          numberOfCols: 12,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        },
-        {
-          breakpoint: "lg",
-          breakpointWidth: 1200,
-          numberOfCols: 10,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        },
-        {
-          breakpoint: "md",
-          breakpointWidth: 996,
-          numberOfCols: 8,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        },
-        {
-          breakpoint: "sm",
-          breakpointWidth: 768,
-          numberOfCols: 4,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        },
-        {
-          breakpoint: "xs",
-          breakpointWidth: 480,
-          numberOfCols: 2,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        },
-        {
-          breakpoint: "xxs",
-          breakpointWidth: 0,
-          numberOfCols: 1,
-          items: [
-            {
-              id: "1",
-              x: 0,
-              y: 0,
-              width: 1,
-              height: 1
-            }
-          ]
-        }
-      ]
+      layouts: layouts
     };
   },
   mounted() {
---
+++ addons/packages/common/dataItems.js
@@ -0,0 +1,86 @@
+let layouts = [
+  {
+    breakpoint: "xl",
+    numberOfCols: 12,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  },
+  {
+    breakpoint: "lg",
+    breakpointWidth: 1200,
+    numberOfCols: 10,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  },
+  {
+    breakpoint: "md",
+    breakpointWidth: 996,
+    numberOfCols: 8,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  },
+  {
+    breakpoint: "sm",
+    breakpointWidth: 768,
+    numberOfCols: 4,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  },
+  {
+    breakpoint: "xs",
+    breakpointWidth: 480,
+    numberOfCols: 2,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  },
+  {
+    breakpoint: "xxs",
+    breakpointWidth: 0,
+    numberOfCols: 1,
+    items: [
+      {
+        id: "1",
+        x: 0,
+        y: 0,
+        width: 1,
+        height: 1
+      }
+    ]
+  }
+];
+export { layouts };

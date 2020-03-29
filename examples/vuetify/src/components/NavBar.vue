6c868af9e3c3c69ad0baf890188744b92916d46c
bensladden <ben.sladden@dematec.com.au>
2020-03-29 10:43:17 +1030

    vuetify setup

---
+++ examples/vuetify/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-app-bar-nav-icon />
+    <v-toolbar-title>Vue Responsive Dash</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- examples/vuetify/src/main.js
+++ examples/vuetify/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");

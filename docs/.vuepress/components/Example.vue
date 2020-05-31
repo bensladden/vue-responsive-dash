1c25d278e84a679b044554d9f0f460aa27a68f8a
bensladden <ben.sladden@dematec.com.au>
2020-05-31 21:44:50 +0930

    docs tidyup

--- docs/.vuepress/components/Example.vue
+++ docs/.vuepress/components/Example.vue
@@ -14,7 +14,7 @@
           <div class="content"></div>
 
           <template v-slot:resizeBottomRight>
-            <svg width="1em" height="1em" viewBox="0 0 20 20" focusable="false" role="img" alt="icon" xmlns="http://www.w3.org/2000/svg" fill="#42b983" class="b-icon bi bi-arrow-down-right mx-auto" data-v-11c9e491=""><g data-v-11c9e491=""><path fill-rule="evenodd" d="M14 9.5a.5.5 0 01.5.5v5a.5.5 0 01-.5.5H9a.5.5 0 010-1h4.5V10a.5.5 0 01.5-.5z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.646 5.646a.5.5 0 01.708 0l9 9a.5.5 0 01-.708.708l-9-9a.5.5 0 010-.708z" clip-rule="evenodd"></path></g></svg>
+            <svg width="1em" height="1em" viewBox="0 0 30 30" focusable="false" role="img" alt="icon" xmlns="http://www.w3.org/2000/svg" fill="#42b983" class="b-icon bi bi-arrow-down-right mx-auto" data-v-11c9e491=""><g data-v-11c9e491=""><path fill-rule="evenodd" d="M14 9.5a.5.5 0 01.5.5v5a.5.5 0 01-.5.5H9a.5.5 0 010-1h4.5V10a.5.5 0 01.5-.5z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.646 5.646a.5.5 0 01.708 0l9 9a.5.5 0 01-.708.708l-9-9a.5.5 0 010-.708z" clip-rule="evenodd"></path></g></svg>
           </template>
         </Dash-Item>
       </Dash-Layout>
